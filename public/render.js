3e2e623c7326f7d754098b81a0cb1b9f138d333e
kapiso-TW <000617jason@gmail.com>
2024-12-06 00:28:09 +0800

    js fix

--- public/render.js
+++ public/render.js
@@ -55,7 +55,7 @@ socket.on('retractMessage', (messageId) => {
         msgElement.textContent = '[訊息已收回]';
         msgElement.classList.add('retracted');
 
-        const retractButton = msgWrapper.querySelector('.recall-button');
+        const retractButton = msgWrapper.querySelector('.tooltip');
         if (retractButton) {
             retractButton.remove();
         }
